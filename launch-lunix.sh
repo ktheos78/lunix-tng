49b931495df48d4b6c1e0466849542206bc4dbdb
root <theosguitar@gmail.com>
2025-11-12 14:13:03 +0000

    add launch script

---
+++ launch-lunix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# compile lunix
+make
+
+# insert lunix module into kernel
+echo "Inserting Lunix module..."
+insmod lunix.ko
+
+# create lunix /dev files
+echo "Creating Lunix /dev files..."
+./mk-lunix-devs.sh
+
+# attach lunix line discipline to ttyS1
+echo "Attaching Lunix line discipline to ttyS1..."
+./lunix-attach /dev/ttyS1
+
+echo "Done."
